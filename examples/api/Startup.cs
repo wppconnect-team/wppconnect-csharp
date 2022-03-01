be838ad615e9d469ca9d681720838fd67aa7bc7e
Rener Lemes <renerlemess@gmail.com>
2022-03-01 18:44:20 -0300

    Projeto de exemplo de API

---
+++ examples/api/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Converters;
+
+namespace WPPConnect.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WPPConnect.API", Version = "v1" });
+            });
+
+            services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+
+            services.AddSingleton<WPPConnectInstance>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WPPConnect.API v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ examples/api/WPPConnectInstance.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+
+namespace WPPConnect.API
+{
+    public class WPPConnectInstance
+    {
+        private readonly WPPConnect.WPPConnection _WPPConnection;
+
+        public WPPConnectInstance()
+        {
+            WPPConnect.Models.Config config = new WPPConnect.Models.Config()
+            {
+                Headless = false,
+                Version = WPPConnect.Models.Enum.LibVersion.Nightly
+            };
+
+            _WPPConnection = new WPPConnect.WPPConnection(config);
+        }
+
+        public async Task SessionCreate(string sessionName, WPPConnect.Models.Token token)
+        {
+            await _WPPConnection.CreateSession(sessionName, token);
+        }
+
+        public async Task<Models.Session> SessionQrCode(string sessionName)
+        {
+            Models.Client client = _WPPConnection.Client(sessionName);
+
+            Models.Session session = await client.QrCode();
+
+            return session;
+        }
+
+        public async Task<Models.Session> SessionStatus(string sessionName)
+        {
+            Models.Client client = _WPPConnection.Client(sessionName);
+
+            Models.Session session = await client.Status();
+
+            return session;
+        }
+
+        public async Task<Models.Session> SessionLogout(string sessionName)
+        {
+            Models.Client client = _WPPConnection.Client(sessionName);
+
+            Models.Session session = await client.Disconnect();
+
+            return session;
+        }
+
+        public async Task<Models.Token> SessionToken(string sessionName)
+        {
+            Models.Client client = _WPPConnection.Client(sessionName);
+
+            Models.Token token = await client.Token();
+
+            return token;
+        }
+    }
+}
